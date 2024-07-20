7fd49ab938227186e2cf115942438915c222b344
vivek70137 <112051210+vivek70137@users.noreply.github.com>
2024-07-20 12:48:33 +0530

    Update dupicate.py

--- t.py
+++ t.py
@@ -1,15 +1,16 @@
-n=["sampath"]
-k=1
-for i in range(len(n)-1):
-    for j in range(i):
-        if n[i]==n[j] and k>=1:
-            k=k+1
-            print('k')
-        k=1
-        
-else:
-    print('gg')
+n = ["sampaaath", "sampaaath", "another", "another", "another"]
+k = 0
 
+# Use a set to keep track of seen elements
+seen = set()
+
+for i in range(len(n)):
+    if n[i] in seen:
+        k += 1
+    else:
+        seen.add(n[i])
+
+print(k)
     
             
             
