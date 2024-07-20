35287a64346904e4b67208720d95e6509a7c9edc
vivek70137 <112051210+vivek70137@users.noreply.github.com>
2024-07-20 13:45:45 +0530

    Update equlibrium.py

--- equlibrium.py
+++ equlibrium.py
@@ -10,3 +10,15 @@ for i,n in enumerate(a):
     
 else:
     print(-1)
+    
+a=[1,2,3,2,1]#or
+r=sum(a)
+l=0
+for i in range(len(a)):
+    r-=a[i]
+    if l==r:
+        print(i)
+        break
+    l+=a[i]
+else:
+    print(-1)
